9c8e9bd6e2f2a67f7dbfec4e96ea7a93a2001be5
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2019-12-19 21:19:14 +0200

    partials location  at new emp

--- src/Components/Partials/_Location.js
+++ src/Components/Partials/_Location.js
@@ -25,43 +25,29 @@ class Basics extends React.Component {
       }
       return (
       <React.Fragment>
-                <Form.Item style={styles.FormI} label="Code">
-                  {getFieldDecorator('Code', {
-                    rules: [{ required: true, message: 'Please input Code Number!' }],
-                  })(<InputNumber placeholder="Type Code" style={styles.inputN} min={0} max={999}/>)}
+                <Form.Item style={styles.FormI} label="Country">
+                  {getFieldDecorator('Country', {
+                    rules: [{ required: true, message: 'Please type Country!' }],
+                  })(<Input placeholder="Type Country" />)}
                 </Form.Item>
-                <Form.Item style={styles.FormI} label="Employee Name">
-                  {getFieldDecorator('EmpName', {
-                    rules: [{ required: true, message: 'Please type employee name!' }],
-                  })(<Input placeholder="Type Employee Name" />)}
+                <Form.Item style={styles.FormI} label="City">
+                  {getFieldDecorator('City', {
+                    rules: [{ required: true, message: 'Please type City!' }],
+                  })(<Input placeholder="Type City" />)}
                 </Form.Item>
-                <Form.Item style={styles.FormI} label="ID Type" hasFeedback>
-                  {getFieldDecorator('IDType', {
-                    rules: [{ required: true, message: 'Please select Id Type!' }],
-                  })(
-                    <Select placeholder="Please select Id Type">
-                      <Option value="it type 1">it type 1</Option>
-                      <Option value="it type 2">it type 2</Option>
-                      <Option value="it type 3">it type 3</Option>
-                      <Option value="it type 4">it type 4</Option>
-                    </Select>,
-                  )}
+                <Form.Item style={styles.FormI} label="Region">
+                  {getFieldDecorator('Region', {})(<Input placeholder="Type Region" />)}
                 </Form.Item>
-                <Form.Item style={styles.FormI} label="ID Number">
-                  {getFieldDecorator('IDNum', {
-                    rules: [{ required: true, message: 'Please type Id Number!' }],
-                  })(<InputNumber placeholder="Type ID Number" style={styles.inputN}  max={99999999999999} />)}
+                <Form.Item style={styles.FormI} label="Postal Code">
+                  {getFieldDecorator('PostalCode', {})(<Input placeholder="Type Postal Code" />)}
                 </Form.Item>
-                <Form.Item style={styles.FormI} label="Phone">
-                  {getFieldDecorator('Phone', {
-                    rules: [{ required: true, message: 'Please type Phone Number!' }],
-                  })(<InputNumber placeholder="Type Phone" style={styles.inputN} max={99999999999999} />)}
+                <Form.Item style={styles.FormI} label="Adress Line 1">
+                  {getFieldDecorator('AdressLine1', {})(<Input placeholder="Type Adress Line 1" />)}
                 </Form.Item>
-                <Form.Item style={styles.FormI} label="Mobile">
-                  {getFieldDecorator('Mobile', {
-                    rules: [{ required: true, message: 'Please type Mobile Number!' }],
-                  })(<InputNumber placeholder="Type Mobile" style={styles.inputN} max={99999999999999}/>)}
+                <Form.Item style={styles.FormI} label="Adress Line 2">
+                  {getFieldDecorator('AdressLine2', {})(<Input placeholder="Type Adress Line 2" />)}
                 </Form.Item>
+                
       </React.Fragment>
         );
     }
